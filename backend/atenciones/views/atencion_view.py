1b24025aa95ef08761e445f3cf6127c4a2006333
Nicolas Cordoba <niccord33@gmail.com>
2026-06-21 18:44:30 -0500

    fix: override initialize_request instead of dispatch to fix mock auth

--- backend/atenciones/views/atencion_view.py
+++ backend/atenciones/views/atencion_view.py
@@ -80,10 +80,11 @@ class AtencionListCreateView(APIView):
             return [AllowAny()]
         return super().get_permissions()
 
-    def dispatch(self, request, *args, **kwargs):
-        if not request.user or not request.user.is_authenticated:
-            request.user = _get_mock_user("1", "CONSULTOR")
-        return super().dispatch(request, *args, **kwargs)
+    def initialize_request(self, request, *args, **kwargs):
+        drf_request = super().initialize_request(request, *args, **kwargs)
+        if not drf_request.user or not drf_request.user.is_authenticated:
+            drf_request.user = _get_mock_user("1", "CONSULTOR")
+        return drf_request
 
     @extend_schema(operation_id="atenciones_list", responses={200: dict})
     def get(self, request):
@@ -111,10 +112,11 @@ class AtencionListCreateView(APIView):
 class AtencionDetailView(APIView):
     permission_classes = [IsAuthenticated]
 
-    def dispatch(self, request, *args, **kwargs):
-        if not request.user or not request.user.is_authenticated:
-            request.user = _get_mock_user("1", "CONSULTOR")
-        return super().dispatch(request, *args, **kwargs)
+    def initialize_request(self, request, *args, **kwargs):
+        drf_request = super().initialize_request(request, *args, **kwargs)
+        if not drf_request.user or not drf_request.user.is_authenticated:
+            drf_request.user = _get_mock_user("1", "CONSULTOR")
+        return drf_request
 
     @extend_schema(
         operation_id="atenciones_retrieve", responses={200: AtencionOutputSerializer}
@@ -127,8 +129,9 @@ class AtencionDetailView(APIView):
 class AtencionProgramarView(APIView):
     permission_classes = [IsAuthenticated, IsOwnerConsultorOrCoordinador]
 
-    def dispatch(self, request, *args, **kwargs):
-        if not request.user or not request.user.is_authenticated:
+    def initialize_request(self, request, *args, **kwargs):
+        drf_request = super().initialize_request(request, *args, **kwargs)
+        if not drf_request.user or not drf_request.user.is_authenticated:
             pk = kwargs.get("pk")
             if pk:
                 try:
@@ -141,10 +144,10 @@ class AtencionProgramarView(APIView):
                     mock_id = (
                         first_consultant.consultant_id if first_consultant else "1"
                     )
-                    request.user = _get_mock_user(mock_id, "CONSULTOR")
+                    drf_request.user = _get_mock_user(mock_id, "CONSULTOR")
                 except Atention.DoesNotExist:
                     pass
-        return super().dispatch(request, *args, **kwargs)
+        return drf_request
 
     @extend_schema(
         summary="Programar/Reagendar fecha y hora de atención",
@@ -185,8 +188,9 @@ class AtencionProgramarView(APIView):
 class AtencionFinalizarView(APIView):
     permission_classes = [IsAuthenticated, IsConsultor, IsOwnerConsultorOrCoordinador]
 
-    def dispatch(self, request, *args, **kwargs):
-        if not request.user or not request.user.is_authenticated:
+    def initialize_request(self, request, *args, **kwargs):
+        drf_request = super().initialize_request(request, *args, **kwargs)
+        if not drf_request.user or not drf_request.user.is_authenticated:
             pk = kwargs.get("pk")
             if pk:
                 try:
@@ -199,10 +203,10 @@ class AtencionFinalizarView(APIView):
                     mock_id = (
                         first_consultant.consultant_id if first_consultant else "1"
                     )
-                    request.user = _get_mock_user(mock_id, "CONSULTOR")
+                    drf_request.user = _get_mock_user(mock_id, "CONSULTOR")
                 except Atention.DoesNotExist:
                     pass
-        return super().dispatch(request, *args, **kwargs)
+        return drf_request
 
     @extend_schema(
         request=FinalizarAtencionInputSerializer,
@@ -224,10 +228,11 @@ class AtencionFinalizarView(APIView):
 class AtencionAnularView(APIView):
     permission_classes = [IsAuthenticated, IsCoordinador]
 
-    def dispatch(self, request, *args, **kwargs):
-        if not request.user or not request.user.is_authenticated:
-            request.user = _get_mock_user("1", "COORDINADOR")
-        return super().dispatch(request, *args, **kwargs)
+    def initialize_request(self, request, *args, **kwargs):
+        drf_request = super().initialize_request(request, *args, **kwargs)
+        if not drf_request.user or not drf_request.user.is_authenticated:
+            drf_request.user = _get_mock_user("1", "COORDINADOR")
+        return drf_request
 
     @extend_schema(
         request=AnularAtencionInputSerializer, responses={200: AtencionOutputSerializer}
@@ -242,10 +247,11 @@ class AtencionAnularView(APIView):
 class AtencionVerificarCruceView(APIView):
     permission_classes = [IsAuthenticated]
 
-    def dispatch(self, request, *args, **kwargs):
-        if not request.user or not request.user.is_authenticated:
-            atencion_id = request.GET.get("atencion_id")
-            consultor_id = request.GET.get("consultor_id")
+    def initialize_request(self, request, *args, **kwargs):
+        drf_request = super().initialize_request(request, *args, **kwargs)
+        if not drf_request.user or not drf_request.user.is_authenticated:
+            atencion_id = drf_request.query_params.get("atencion_id")
+            consultor_id = drf_request.query_params.get("consultor_id")
             mock_id = "1"
             if atencion_id:
                 try:
@@ -257,8 +263,8 @@ class AtencionVerificarCruceView(APIView):
                     pass
             elif consultor_id:
                 mock_id = consultor_id
-            request.user = _get_mock_user(mock_id, "CONSULTOR")
-        return super().dispatch(request, *args, **kwargs)
+            drf_request.user = _get_mock_user(mock_id, "CONSULTOR")
+        return drf_request
 
     @extend_schema(
         parameters=[VerificarCruceInputSerializer],
