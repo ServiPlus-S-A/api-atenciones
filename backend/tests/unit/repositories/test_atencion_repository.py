6e5c9c215ac6a0af776472bd120cf14b94622fde
Nicolas Cordoba <niccord33@gmail.com>
2026-06-23 21:20:27 -0500

    test: ruff format aplied

--- backend/tests/unit/repositories/test_atencion_repository.py
+++ backend/tests/unit/repositories/test_atencion_repository.py
@@ -77,6 +77,7 @@ def test_listar_filtros_estado_y_request_id():
 @pytest.mark.django_db
 def test_listar_filtros_hu12_cliente_consultor_solicitud_y_fecha_registro():
     from django.utils import timezone as dj_timezone
+
     atencion = AtencionFactory(request_id=123, customer_name="Ana Maria Rojas")
     AtentionConsultant.objects.create(
         atention=atencion,
